17a17c8423c60a35c02751ac26ec0b3f06210d3f
Azamat <aa@toolpar.com>
2022-11-14 22:32:10 +0600

    add2

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const html5QrCode = new Html5Qrcode("qr-reader");
 
+var modal = document.getElementById("modal");
+var vendID = document.getElementById("vending_id");
+var phone = document.getElementById("phone");
+var whatsapp = document.getElementById("whatsapp");
+
 let config = {
     fps: 10,
     rememberLastUsedCamera: true,
     supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
 };
 
+
+function showModal() {
+    vendID.innerHTML = "dsdsd";
+    phone.href = "tel:+77776420344";
+    whatsapp.href = "https://wa.me/77776420344";
+    modal.classList.add("show");
+}
+
+function closeModal() {
+    modal.classList.remove("show");
+}
+
 function onScanSuccess(decodedText, decodedResult) {
-    console.log(`Code matched = ${decodedText}`, decodedResult);
+    showModal();
 }
 
 function onPermissionSuccess(devices) {
@@ -21,3 +38,4 @@ function onPermissionError(error) {
 }
 
 Html5Qrcode.getCameras().then(onPermissionSuccess, onPermissionError);
+
