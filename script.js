4e78d40541a729dd4bb885f302d79762c73bcaef
Azamat <aa@toolpar.com>
2022-11-14 23:13:42 +0600

    add2

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var modal = document.getElementById("modal");
 var vendID = document.getElementById("vending_id");
 var phone = document.getElementById("phone");
 var whatsapp = document.getElementById("whatsapp");
+var contact = document.getElementById("contacts");
+var loader = document.getElementById("loader");
 
 let config = {
     fps: 10,
@@ -12,19 +14,38 @@ let config = {
 };
 
 
-function showModal() {
-    vendID.innerHTML = "dsdsd";
-    phone.href = "tel:+77776420344";
-    whatsapp.href = "https://wa.me/77776420344";
+function showModal(decodedText) {
+    contact.classList.add("d-none");
+    loader.classList.remove("d-none");
     modal.classList.add("show");
+    var uuid = decodedText.replaceAll('https://kaspi.kz/pay/Smartvend?service_id=4680&7363=', '');
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://partner.smartvend.kz/api/partner/support/" + uuid + "/");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+
+            let data = JSON.parse(xhr.responseText);
+
+            loader.classList.add("d-none");
+            contact.classList.remove("d-none");
+            vendID.innerHTML = data.vending_id;
+            phone.href = "tel:+7" + data.support_phone;
+            whatsapp.href = "https://wa.me/7" + data.support_phone;
+        }
+    };
+
+
 }
 
 function closeModal() {
     modal.classList.remove("show");
 }
 
+
+
+
 function onScanSuccess(decodedText, decodedResult) {
-    showModal();
+    showModal(decodedText);
 }
 
 function onPermissionSuccess(devices) {
